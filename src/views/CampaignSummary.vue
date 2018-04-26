33fb9223f4de22dccb5f8adafcd394f5ec911bf1
Chris Jennison <cjennison92@gmail.com>
2018-04-26 17:02:07 -0400

    add some meat to the dashboard

--- src/views/CampaignSummary.vue
+++ src/views/CampaignSummary.vue
@@ -2,6 +2,44 @@
   #campaign_summary
     v-subheader Summary
     h1.display-2 {{ campaign.name }}
+    .story.pa-3(style='text-align:left')
+      v-card.pa-3
+        v-layout
+          v-flex(xs12)
+            v-card-title The Story So Far
+        v-layout  
+          v-flex(xs12)
+            .story
+              v-alert(type="warning" :value="true") You haven't completed a segment yet! Complete a segment to write your story.
+    v-layout
+
+      v-flex.pa-3(xs12 sm4)
+        v-card
+          v-card-title 
+            span.headline Segments
+          hr
+          v-card-title You don't have any segments yet!
+          v-card-actions 
+            v-btn View
+            v-btn Create New
+      v-flex.pa-3(xs12 sm4)
+        v-card
+          v-card-title 
+            span.headline Monsters
+          hr
+          v-card-title You don't have any monsters yet!
+          v-card-actions 
+            v-btn View
+            v-btn Create New
+      v-flex.pa-3(xs12 sm4)
+        v-card
+          v-card-title 
+            span.headline Items
+          hr
+          v-card-title You don't have any monsters yet!
+          v-card-actions 
+            v-btn View
+            v-btn Create New
 </template>
 
 <script>
